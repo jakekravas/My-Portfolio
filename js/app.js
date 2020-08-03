4f8857f5b8acedbdcd13d9df8ea6bf5d1c55d508
Jake Kravas <jakekravas7@gmail.com>
2020-08-02 19:40:52 -0700

    navbar hides on scroll

---
+++ js/app.js
@@ -0,0 +1,22 @@
+let prevScrollpos = window.pageYOffset;
+
+if (prevScrollpos < 5) {
+  document.getElementById("nav").style.boxShadow = "none";
+}
+
+console.log(prevScrollpos);
+window.onscroll = () => {
+  let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+    document.getElementById("nav").style.boxShadow = "rgba(2, 12, 27, 0.7) 0px 10px 30px -10px";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+    document.getElementById("nav").style.boxShadow = "none";
+  }
+  prevScrollpos = currentScrollPos;
+  if (prevScrollpos < 5) {
+    document.getElementById("nav").style.boxShadow = "none";
+  }
+}
