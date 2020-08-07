6ac5af154b5d1b06a8bf0028f70461d1b80cc9a2
Jake Kravas <jakekravas7@gmail.com>
2020-08-07 04:24:03 -0500

    Added scrollspy and smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,21 +1,61 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      let hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // 800 = number of milliseconds it takes to scroll to specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
 let prevScrollpos = window.pageYOffset;
 
+// Removing box shadow of navbar if scroll position is at or near the top
 if (prevScrollpos < 5) {
   document.getElementById("nav").style.boxShadow = "none";
 }
 
 window.onscroll = () => {
+  // setting value to the current vertical scroll position of page
   let currentScrollPos = window.pageYOffset;
 
+  // If scrolling up, show the navbar
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("nav").style.top = "0";
     document.getElementById("nav").style.boxShadow = "rgba(2, 12, 27, 0.7) 0px 10px 30px -10px";
   } else {
+    // If scrolling down, hide the navbar
     document.getElementById("nav").style.top = "-50px";
     document.getElementById("nav").style.boxShadow = "none";
   }
   prevScrollpos = currentScrollPos;
+
+  // Removing navbar box shadow if vertical scroll position isn't near the top
   if (prevScrollpos < 5) {
     document.getElementById("nav").style.boxShadow = "none";
   }
 }
+
+// let offset = 80;
+
+// $('.navbar li a').click(function(event) {
+//     event.preventDefault();
+//     console.log($($(this).attr('href'))[0]);
+//     $($(this).attr('href'))[0].scrollIntoView();
+//     scrollBy(0, -offset);
+// });
